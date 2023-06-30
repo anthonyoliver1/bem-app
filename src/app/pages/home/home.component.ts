47de8e03b3be69fec79dad9d7660d5f9bd8e94d2
Anthony Oliveira <anthony.silvaoliveira@outlook.com>
2023-06-30 09:45:30 -0300

    feat(home): adicionando conteúdo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public codeExemple1: string =
+    `
+    <ul class="list">
+      <li class="list__item">
+        <h2 class="list__title">My publication</h2>
+        <p class="list__author>Julio Lozovei</p>
+        <p class="list__text">A publication using BEM :)</p>
+      </li>
+    </ul>
+  `;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
